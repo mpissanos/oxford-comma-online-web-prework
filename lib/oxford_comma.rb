65a1f40e29b800ddfef4728fcd4cd6dae10571b7
Manny Pissanos <mpissano@gmail.com>
2018-09-10 13:33:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+i= 0
+string = ""
+if array.length == 1
+  return array.join
+else if array.length == 2
+return "#{array[0]} and #{array[1]}"
+  else 
+    until i == array.length - 1
+      string << "#{array[i]}, "
+      i += 1
+    end
+    string << "and #{array[array.length-1]}"
+    return string
+    end
+  end
 end
