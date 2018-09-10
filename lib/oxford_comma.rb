49ba4577e6554393b5ca95d61cf0a7577175178c
Manny Pissanos <mpissano@gmail.com>
2018-09-10 13:27:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,12 @@ if array.length == 1
   return array.join
 else if array.length == 2
 array.join(" and ")
-else array .length > 2
-  string = ""
-  return string << "and "
-  
+else 
+    until i == array.length - 1
+      string << "#{array[i]}, "
+      i += 1
     end
+    string << "and #{array[array.length-1]}"
+    return string
   end
 end
