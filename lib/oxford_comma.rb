d4b6b030e9f08bc22e51203c07ffd9877afa7d35
Manny Pissanos <mpissano@gmail.com>
2018-09-10 13:32:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,3 @@ if array.length == 1
   return array.join
 else if array.length == 2
 return "#{array[0]} and #{array[1]}"
-    until i == array.length - 1
-      string << "#{array[i]}, "
-      i += 1
-    end
-   return string << "and #{array[array.length-1]}"
-   end
-  end
-end
