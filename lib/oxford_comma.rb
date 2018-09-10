2a2b2100ee006b07bb730c84517346021a8ae2a4
Manny Pissanos <mpissano@gmail.com>
2018-09-10 13:30:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.length == 1
   return array.join
 else if array.length == 2
-return "#array[0] and #{array[1]}"
+return "#{array[0]} and #{array[1]}"
     until i == array.length - 1
       string << "#{array[i]}, "
       i += 1
